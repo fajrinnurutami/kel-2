490d4c242d00e616456644c4aea79f818ee045cb
fajrinnurutami <fajrin145@gmail.com>
2021-05-01 18:36:14 +0700

    delete login

--- src/components/Login.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Button, Container, Row, Col } from 'react-bootstrap';
-
-const About = () =>{
-    return (
-        
